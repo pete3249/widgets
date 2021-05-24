3a27daf277f0f04de5c9f3786a13f2337e27d03e
pete3249 <katelynrpeterson16@gmail.com>
2021-05-23 20:16:00 -0400

    Add async request

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,41 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('')
+    const [term, setTerm] = useState('programming')
+    const [results, setResults] = useState([])
 
+    console.log(results)
+
+    // do not try to mark function as async, will get error
     useEffect(() => {
-        
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+            setResults(data.query.search);
+        };
+
+        search();
     }, [term])
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    {result.snippet}
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div>
@@ -20,6 +50,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     )
 }
