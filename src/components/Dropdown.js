638ddef9c5a782fe3e7fccc7f5aecffcdc6c0797
pete3249 <katelynrpeterson16@gmail.com>
2021-05-25 13:43:40 -0400

    Add toggle dropdown button

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState('false')
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                    return
+                }
+            setOpen(false);
+        }
+
+        document.body.addEventListener('click', onBodyClick, { capture: true }) 
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick, { capture: true })
+        };
+    }, []);
 
     const renderedOptions = options.map(option => {
         if (option.value === selected.value) {
@@ -18,7 +34,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         )
     })
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 <div 
