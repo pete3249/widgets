7885c151888dba7dffbc42cdb25c51a19281daec
pete3249 <katelynrpeterson16@gmail.com>
2021-05-25 17:10:15 -0400

    Add translate functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import Accordion from './components/Accordion';
 // import Search from './components/Search';
-import Dropdown from './components/Dropdown';
+// import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
     {
@@ -34,16 +35,10 @@ const options = [
 ]
 
 const App = () => {
-    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            {/* <Accordion items={items} /> */}
-            {/* <Search /> */}
-            <Dropdown 
-                selected={selected} 
-                onSelectedChange={setSelected} 
-                options={options} 
-            />
+          <Translate />
         </div>
     );
 };
