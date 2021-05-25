0f3d004112b3493fcb56eb9f6933e6809d24c152
pete3249 <katelynrpeterson16@gmail.com>
2021-05-24 22:16:57 -0400

    Add option array and Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import Accordion from './components/Accordion';
+import React, { useState } from 'react';
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -16,10 +18,32 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    }, 
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Shade of Blue',
+        value: 'blue'
+    }
+]
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
-            <Accordion items={items} />
+            {/* <Accordion items={items} /> */}
+            {/* <Search /> */}
+            <Dropdown 
+                selected={selected} 
+                onSelectedChange={setSelected} 
+                options={options} 
+            />
         </div>
     );
 };
